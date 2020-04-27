a73b81c392f277b56fa15bbcad07740414f51336
katanaevav <katanaev@gmail.com>
2020-04-27 23:35:47 +0400

    Выполнено задание 18. Учебный проект: большие перемены (Часть 1)

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,7 +4,7 @@ const castTimeFormat = (value) => {
   return value < 10 ? `0${value}` : String(value);
 };
 
-const formatTime = (date) => {
+export const formatTime = (date) => {
   const hours = castTimeFormat(date.getHours() % 12);
   const minutes = castTimeFormat(date.getMinutes());
 
