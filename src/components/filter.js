f93263da35421c4f1535774f79219cd3e9b219da
katanaevav <katanaev@gmail.com>
2020-05-06 02:18:42 +0400

    Выполнено задание 22. Учебный проект: меняй-удаляй (часть 1)

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,5 +1,11 @@
 import AbstractComponent from "./abstract-component.js";
 
+const FILTER_ID_PREFIX = `filter__`;
+
+const getFilterNameById = (id) => {
+  return id.substring(FILTER_ID_PREFIX.length);
+};
+
 const createFilterMarkup = (filter, isChecked) => {
   const {name, count} = filter;
 
@@ -18,22 +24,28 @@ const createFilterMarkup = (filter, isChecked) => {
 };
 
 const createFilterTemplate = (filters) => {
-  const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
+  const filtersMarkup = filters.map((it) => createFilterMarkup(it, it.checked)).join(`\n`);
 
-  return (
-    `<section class="main__filter filter container">
-      ${filtersMarkup}
-    </section>`
-  );
+  return `<section class="main__filter filter container">
+    ${filtersMarkup}
+  </section>`;
 };
 
 export default class Filter extends AbstractComponent {
   constructor(filters) {
     super();
+
     this._filters = filters;
   }
 
   getTemplate() {
     return createFilterTemplate(this._filters);
+  }
+
+  setFilterChangeHandler(handler) {
+    this.getElement().addEventListener(`change`, (evt) => {
+      const filterName = getFilterNameById(evt.target.id);
+      handler(filterName);
+    });
   }
 }
